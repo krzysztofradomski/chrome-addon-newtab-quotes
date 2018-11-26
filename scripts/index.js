22904392ddd78d652cabe163145ca41ecd3b3055
Krzysztof Radomski <krzysztofradomski@krs.local>
2018-11-26 20:44:02 +0100

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,199 @@
+      var bomba_quotes = ["– (Chorąży Torpeda przez krótkofalówkę) Bomba! Bomba! Odezwij się!\n– Nie gadam z pedałami!\n– Ja też! Ale mimo to zadzwoniłem! O kurwa! Przestańcie jebać z tego moździerza bo wszystko spada na nas!\n– Twoje dupsko nie takie rzeczy już brało! Bez odbioru. (do Janusza, który operuje moździerzem) Zorientowali się. Dobra, celuj w fabrykę.","– Co to jest?\n– Czekaj, przybliżę.\n– O kurwa, Moto-Komando.\n– Raczej Homo-Komando.","– Czy można wiedzieć za kim pan jest?\n– Jeżeli chodzi o futbol można powiedzieć, że jestem jaroszem.\n– O ty kurwa twoja mać, a co ci się w RKS CHUWDU nie podoba, fiucie jeden?","– Do rzeczy, co ci mówi nazwa Napletek?\n– Planeta Napletek, plantacja osadników.\n– Chodziło mi raczej o tą skórkę na fiucie.","Donna mamma es huiocita.","– Dobra tępe chuje, są jakieś pytania?\n– Kurwa, nic nie zrozumiałem.\n– Pewnie dlatego że jesteś tępym chujem.\n– Panie kapitanie. Ja mam pytanie.\n– Kurwa, to pytaj.\n– Kurwa. Panie Kapitanie. Jak wylądujemy na słońcu to się spalimy.\n– Tępy chuju. Masz mnie za idiotę? Wylądujemy w nocy.\n– Kurwa, teraz wiem dlaczego jest Pan jest kapitanem.","– Halo?\n– W szkole jest bomba.\n– Kto mówi?\n– Damian Wąsik.\n– Kamil, pojebało cię?\n– Nieważne kto, na pewno nie Kamil Zdun.","Je i sra.\nOpis: Kamil Zdun przybliżający proces trawienia Skola\n","Koniec uroczystości. Wracajcie kraść rowery, układać tarota... czy co tam jeszcze robią Cyganie.\nOpis: żona króla cygańskiego podczas pogrzebu męża\n","– Kto im dał pozwolenie na moździerz?\n– Chuj!\n– Chuj?\n– Chuj z nimi, mamy towarzystwo.","– Kurwa twoja mać, jak kameleon! Niewidzialny! Za to ja was widzę bardzo dobrze! I myk, bombą zegarową.\n– O kurwa, zaraz zginiemy!\n– Spokojnie. Na ile ustawiony jest zegar?\n– O kurwa, to trzeba coś ustawić? Czekaj. Hm, „On, Off”. Kurwa, po angielsku!\n– Karabiny w dupę i napierdalać!\n– Zaraz, gdzieś tu słowniczek miałem.","– Mamy wpierdol.\n– Jak to, kurwa, za co?\n– Tępy chuju, ten kosmita tak się nazywa.","– Masz 10 sekund na pożegnanie się z życiem!\n– Gówno mi zrobisz tym swoim gównianym karabinkiem! Nie wiesz idioto, że jestem odporny na kule od karabina?!\n– Nie zginiesz od karabina, rozpierdolę cię granatem!\n– Heh! W dupie mam twój granat!!!\n– Zgadza się!","– NAPIERDALAAAAAAAAĆ!\n– Kurwa, panie kapitanie! Przecież napierdalamy!\n– Żebyś jeszcze tak, jak mordą, napierdalał z karabinu, to byłby z ciebie pożytek! Masz gdzie napierdalać?!\n– (Sebek zauważa, że strzelał w kamień) O kurwa.","– O, a takie pytanie. Panie kapitanie, jak pan myśli, gdyby dupa miała zęby, to co by powiedziała?\n(Głos z głośnika):\n– Zapraszam!\n– Zapraszam?","– O kurwa, miałem zabrać aparat, teraz nikt mi nie uwierzy. Bomba ze swoją załogą, pedały. Zdjęcie bym podpisał „Pedalska załoga kapitana Pedała”, a wasza rakieta powinna nazywać się „Pedał 7”.\n– Torpeda, ty się kurwa od Pedała 7 odpierdol.\n– Orzeła.\n– Orzeła. A pedałem to jesteś ty, twój ojciec, twój dziadek, twój pies, twoi koledzy i koleżanki, i twoja matka. A nie, czekaj, zapomniałem. Twoja matka to chuj. Pedalski.","O kurwa! Szkoda że nie mam... A nie, mam!\nOpis: Kapitan Bomba widząc nagiego Torpedę z kosmitami\n","– Panie kapitanie, a jak jakiś kosmita ma w miejscu oczu jajca, i tak tutaj, zamiast buzi i nosa, taki kutacz, to czy on czuje i widzi?\n– Czuje i widzi.\n– No, ale jak?\n– Chujowo.","– Panie kapitanie, ale żeśmy kurcze przypieprzyli!\n– Co?\n– Panie kapitanie, ale żeśmy przyjebali, że chuj.\n– No!","– Panie kapitanie! Co się dzieje?! Nasze pociski nic mu nie robią!\n– Tępy chuju! On ma pole siłowe!\n– To po co strzelamy?!\n– Ciszej kurwa, myślę!","– Panie kapitanie! Dlaczego pan strzelał? Przecież tam nie było żadnego kosmity!\n– Lubię se czasem tak profilaktycznie ponapierdalać.","– Tępe chuje\n– Kurwa, dlaczego Pan na nas tak mówi\n– Bo jesteście tępymi chujami","– Torpeda, jak śmiesz nazywać mnie ciotą jebany skurwysynu. Moja dupa jest ciasna jak po praniu, a twoja matka to chuj.\n– Bo jesteś kurwa głupi jak but.\n– A ty, ty jesteś głupi jak, jak chuj. Ty chuju.\n– O ty kurwa!","– Ty. Co byś zrobił, gdybyś się dowiedział, że twoja siorka opierdala kiełbachy kosmitom?\n– To samo, co ty, gdybyś się dowiedział, że twój stary ma jedno jajco.\n– Mój stary ma jedno jajco!\n– Spierdalaj!"];
+      var wilq_quotes = [
+        "Boś buc.\nOpis: odpowiedź na pytanie Wilqa, dlaczego nie został zaproszony na sylwestra.",
+        "Chuja tam…\nOpis: odpowiedź na większość kwestii.",
+        "Dziwnym nie jest.\nOpis: odpowiedź na większość kwestii.",
+        "Pizgać się w wodzie łatwo nie jest.\nOpis: scena z pobytu na basenie w Suchym Borze.",
+        "Trener Piechniczek mawiał też: „To było praktycznie, kurwa, trudne”.",
+        "Wiesz, jak liście potrafią szumieć, man? To są chuje, ja je znam.",
+        "W swoim środowisku naturalnym żółw i tuńczyk to są kumple. Razem się bawią, skaczą, przyjaźnią…",
+        "A może żółw nie ma masy mięśniowej? W końcu jest taki mały.",
+        "Będzie burza, bo mi się wydłuża. (na co odpowiada AlcMan: Będzie deszcz, bo mnie też.)",
+        "Buzi, dupci, cipci i drucika.\nOpis: odpowiedź na zadane przez kierownika klubu komiksowego „Kącik” pytanie: „Co dla ciebie, młody człowieku?”",
+        "Całuj mnie w chuja aż się rozbuja.",
+        "Całuj mnie w cyc i nie mów nic.",
+        "Chuj wielki i szelki.",
+        "Ciebie o coś prosić, to jak chujem trawę kosić.",
+        "Idziemy dewastować polityków. Szczególnie tych młodych, ambitnych.",
+        "Jak mawia trener Piechniczek: „Jeśli nie masz czasu na rozgrzewkę – przynajmniej oddychaj przez nos”.",
+        "Jak mawiał trener Piechniczek: „Przegraliśmy mecz, ale mamy piłkę”.",
+        "Jak mawiał trener Piechniczek: „Wiara wierzy w cuda”.",
+        "Jak mawia trener Piechniczek: „Żebyś się nie zesrał”. Bucu!",
+        "Jak to mówią w Rzeszowie: fuck you.",
+        "Kucharz kucharz nie oszukasz",
+        "Na każdej bezludnej wyspie jest jakiś tubylec.\nOpis: spostrzeżenie w trakcie poszukiwań mordercy na kontynentalnym brzegu Opola.",
+        "Nie masz na to odpowiedzi, bo mój chuj ci w gardle siedzi.",
+        "Pytą twojego starego?\nOpis: odpowiedź na zadane mu przez dresiarza podsuwającego mu pięść pod nos pytanie „Wiesz czym to pachnie?”.",
+        "Stare żydowskie przysłowie: młynarz, młynarz, na chuj ci młynarz.",
+        "To był mniejszy wysiłek niż strzepnąć z pyty pyłek.",
+        "Tylko bez grypsery ciamparajo. Nie jestem twoim bambaryłą.",
+        "Żuj pindla!!!\nOpis: do komisarza Gondora.",
+        "Godzina szósta – panie biorą w usta.",
+        "Jak mawia trener Piechniczek „Koszulek macie siedem, ale piłki są trzy”.",
+        "Jak mawia trener Piechniczek „Prawdziwy sportowiec zakłada pokrowiec”.",
+        "Jak mawia trener Piechniczek „Skończyła się szatnia – zaczęła się trawa”.",
+        "Jak mawiał trener Piechniczek „Czasami się zawsze wygrywa, a czasami nigdy”.",
+        "Jak mawiał trener Piechniczek „Czasem tak się zdarza, że piłka leci do piłkarza”.",
+        "Jak mawiał trener Piechniczek „Jak mamy tu coś zwojować, skoro między jedną siatką a drugą jest przerwa na piłkę?”",
+        "Jak mawiał trener Piechniczek „Jeśli chcesz grać w piłkę, musisz myśleć jak piłka”.",
+        "Jak mawiał trener Piechniczek „Najważniejsze, że wziąłem buty”.",
+        "Jak mawiał trener Piechniczek „Nie da się drugi raz wykorzystać niewykorzystanej sytuacji”.",
+        "Jak mawiał trener Piechniczek „Nie ma czasu na fair play”.",
+        "Jak mawiał trener Piechniczek „Prawo Pascala: Kto wykopie – zapierdala”.",
+        "Jak mawiał trener Piechniczek „Taka akcja że aż się sędzia za krocze złapał”.",
+        "Jest różnica w romantyzmie. „Wchodzić” to określenie z harlequina, natomiast „pukać”…\nOpis: odpowiedź na pytanie Wilqa: „No bez jaj! To już się nie puka jak się wchodzi?”.",
+        "Kto buców nie tłuce, ten sam jest bucem!",
+        "Nie możemy podcinać gałęzi na której sramy we własne gniazdo!",
+        "W Krakowie mówią nawet: „Tarz się w gównie byle równie”.",
+        "Złemu tancerzowi prącie w tańcu mrowi.",
+        "Ba, ba, ba.\nOpis: kiedy jest podniecony, czyli zawsze kiedy może coś powiedzieć.",
+        "Pupcia rubcia salcesonik helou belou w dupę słonik!",
+        "Weź ten pająk i go ugryź.\nOpis: kiedy Mikołaj chciał zostać superbohaterem.",
+        "Wyrwę ci serce i naszczam w prawy przedsionek!"
+      ];
+      var h = window.innerHeight;
+      var w = window.innerWidth;
+      var api = 'https://api.unsplash.com/photos/random';
+      var cred = '?client_id=8893199052847965d3999199c4079cdf607d1f854c5485f43822beb93ef124c4';
+      var query = '&query=landscape';
+      var size = '&w=' + w + '&h=' + h;
+      var url = api + cred + query + size;
+      
+      
+      async function start() {
+        var choice = await window.localStorage.getItem('quotes-choices')
+        var source = bomba_quotes.concat(wilq_quotes);
+        if (choice === "bomba") {
+          source = bomba_quotes;
+        }
+        if (choice === "wilq") {
+          source = wilq_quotes;
+        }
+        var sentence = source[Math.floor(Math.random() * (source.length + 1))];
+        document.getElementById('quote').innerText = sentence;
+      }
+
+      function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('time').innerHTML =
+        h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+      }
+
+      function checkTime(i) {
+        if (i < 10) {i = "0" + i};
+        return i;
+      }
+
+      function saveImage(url) {
+        console.log('saveImage');
+        var oldURL = window.localStorage.getItem('next-img-url');
+        if (url && oldURL !== url) {
+          console.log('url', url);
+          console.log('oldURL', oldURL);
+          window.localStorage.setItem('next-img-url', url);
+          var storedImgUrls = window.localStorage.getItem('img-urls') || '';
+          window.localStorage.setItem('img-urls', storedImgUrls + url);
+          var preloadImg = new Image();
+          preloadImg.src = url.replace(/"/g, '');
+          document.body.appendChild(preloadImg);
+        }
+      }
+
+      function getImage() {
+        console.log('getImage');
+        return window.localStorage.getItem('next-img-url');
+      }
+
+      function getRandomOldImage() {
+        console.log('getRandomOldImage');
+        var ls = window.localStorage.getItem('img-urls') || "";
+        var randomOldImages = ls.split('""');
+        return randomOldImages[Math.floor(Math.random() * randomOldImages.length )];
+      }
+
+      function setImage(image) { 
+        console.log('seTImage');
+        var url = "url(" + image + ")";
+        console.log('url', url);
+        document.getElementById('content').style.backgroundImage = url;
+      }
+
+      function setFallbackImage() {
+        window.localStorage.setItem('img-urls', 
+        '"https://images.unsplash.com/photo-1537083702767-42f85f12834f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1520&h=1027&fit=crop&ixid=eyJhcHBfaWQiOjQyMDAzfQ&s=c82ef40fc32e1747cc0fde73d4ec1db2"'
+        );  
+      }
+
+      async function handleImage(url) {
+        console.log('handleImage');
+        try {
+          var img = getImage();
+          if (!img) {
+            saveImage(url);
+            const loaded = await loadImage();
+            console.log('loaded', loaded)
+          } else {
+            setImage(img);
+            saveImage(url);
+          }
+        }
+        catch (e) {
+          console.log('ee', e);
+        }
+      }
+
+      function loadImage() {
+        console.log('loadImage');
+        fetch(url)
+          .then(response => response.status === 200 ? response.json() :  null )
+          .then(json => json ? handleImage(JSON.stringify(json.urls.custom || json.urls.full)) : handleImage(getRandomOldImage()))
+      }
+
+      function handleForm(event) {
+        console.log('handleForm');
+        event.preventDefault();
+        setFallbackImage();
+        var inputs = document.querySelectorAll("input[type='checkbox']"); 
+        var choices = '';
+        for(var i = 0; i < inputs.length; i++) {
+            if (inputs[i].checked) {choices = choices + (inputs[i].name)}
+        }
+
+        window.localStorage.setItem('quotes-choices', choices);
+
+        document.getElementById('choice').style.opacity = '0';
+        document.getElementById('time').style.opacity = '1';
+        document.getElementById('quote').style.opacity = '1';
+        setTimeout(() => {
+          document.getElementById('choice').style.display = "none";
+          document.getElementById('time').style.display = 'block';
+          document.getElementById('quote').style.display = 'block';
+          start();
+        }, 2000);
+      }
+
+      window.onload = function () {
+        document.getElementById('choice').addEventListener('submit', handleForm);
+        try {
+          var choice = null;
+          if (Boolean((window.localStorage.getItem('quotes-choices')))) {
+            document.getElementById('choice').style.display = "none";
+            document.getElementById('time').style.display = 'block';
+            document.getElementById('quote').style.display = 'block';
+            document.getElementById('time').style.opacity = '1';
+            document.getElementById('quote').style.opacity = '1';
+          }
+          start();
+          startTime();
+          loadImage();
+        }
+        catch (error) {
+          console.log(error);
+        }
+      }
